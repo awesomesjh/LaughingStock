904b62c3d9bebef3005a0ae4c41578e5dd024c7b
David <davidtaywd@gmail.com>
2023-05-25 11:24:54 +0800

    username and password form has been added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,47 @@
+import {useState} from 'react'
+
 const Navbar = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleUsername = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleLogin = (event) => {
+    console.log("submitting")
+    event.preventDefault() // prevent page refresh
+    
+    // get username and password
+    console.log(username)
+    console.log(password)
+
+    // clear all inputs on the form
+    setUsername('')
+    setPassword('')
+
+  }
+
   return (
-    <nav>
-      <ul>
-        <li><a href="#">Username</a></li>
-        <li><a href="#">Password</a></li>
-      </ul>
-    </nav>
+    <form onSubmit={handleLogin}>
+       <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={handleUsername}
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          value={password}
+          onChange={handlePassword}
+        />
+      <button type="submit">Login</button>
+    </form>
   )
 }
 
