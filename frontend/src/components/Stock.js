73fd06b27c9ce412871657d5a207e05cd1b14f87
David <davidtaywd@gmail.com>
2023-05-28 12:09:07 +0800

    Changed icon for deleting each stock. You are now able to edit the stock quantity as well

--- frontend/src/components/Stock.js
+++ frontend/src/components/Stock.js
@@ -1,13 +1,27 @@
-const Stock = ({ stock, deleteStock }) => {
+import { BsFillTrashFill } from "react-icons/bs"
+import "./Stock.css"
+
+const Stock = ({ stock, deleteStock, onChangeInput }) => {
   const price = 120
   return (
     <tr>
       <td>{stock.symbol}</td>
-      <td>{stock.quantity}</td>
+      <td>
+        <input
+          name="quantity"
+          value={stock.quantity}
+          type="number"
+          onChange={(e) => onChangeInput(e, stock.symbol)}
+          placeholder="Type Quantity"
+        />
+      </td>
       <td>{price}</td>
       <td>{stock.quantity * price}</td>
       <td>
-        <button type='button' onClick={() => deleteStock(stock.id)}>Delete</button>
+        <span className="actions">
+          <BsFillTrashFill color='red' onClick={() => deleteStock(stock.id)} />
+        </span>
+        {/* <button type='button' onClick={() => deleteStock(stock.id)}>Delete</button> */}
       </td>
     </tr>
   )
