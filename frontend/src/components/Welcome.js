094f1f7cf20cbd4caa4518f10a5219aab652b9f8
David <davidtaywd@gmail.com>
2023-07-06 10:48:09 +0800

    scrolling css

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -4,12 +4,86 @@ import styles from './Welcome.module.css'
 
 const Welcome = () => {
   return (
-    <main className={`${styles.container} background`}>
-      <h2 className={styles.header}>Welcome to Laughing Stock</h2>
-      <Link to='/login'>
-        <Button>Login</Button>
-      </Link>
-    </main>
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <img src={require('../img/background.jpg')} alt='stock background' className={styles.background}></img>
+        {/* use styles.foreground for foreground image */}
+        <div className={styles.container}>
+          <h2 className={styles.welcomeText}>Welcome to Laughing Stock</h2>
+          <Link to='/login'>
+            <Button>Login</Button>
+          </Link>
+        </div>
+      </div>
+
+      <section className={styles.featureSection}>
+        <h1 className={styles.featureTitle}>Features</h1>
+        <div className={styles.featureSet}>
+          <div className={styles.card}>
+            <img src={require('../img/login.png')} alt='login' className={styles.smallImg}></img>
+            <div className={styles.smallIntro}>
+              <h1 className={styles.featureDescription}>Login Functionality</h1>
+              <p className={styles.featureExplanation}>Users can create an account and log in.</p>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <img src={require('../img/telegram.png')} alt='telegram bot' className={styles.smallImg}></img>
+            <div className={styles.smallIntro}>
+              <h1 className={styles.featureDescription}>Telegram Bot</h1>
+              <p className={styles.featureExplanation}>Provides a convenient way for the user to view their stock portfolio on mobile devices</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.featureSet}>
+          <div className={styles.card}>
+            <img src={require('../img/dashboard.png')} alt='dashboard' className={styles.largeImg}></img>
+            <div className={styles.largeIntro}>
+              <h1 className={styles.featureDescription}>Dashboard</h1>
+              <p className={styles.featureExplanation}>Basic CRUD functionality for users to modify their stock portfolio.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.featureSet}>
+          <div className={styles.card}>
+            <img src={require('../img/linechart.png')} alt='linechart' className={styles.largeImg}></img>
+            <div className={styles.largeIntro}>
+              <h1 className={styles.featureDescription}>Line Chart</h1>
+              <p className={styles.featureExplanation}>View change in portfolio value over time.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.featureSet}>
+          <div className={styles.card}>
+            <img src={require('../img/candlestick.png')} alt='candlestick' className={styles.largeImg}></img>
+            <div className={styles.largeIntro}>
+              <h1 className={styles.featureDescription}>Candlestick Chart</h1>
+              <p className={styles.featureExplanation}>View change in price of a selected stock. Bar data is obtained from Alpaca Market data API, and plotted into a candlestick chart using React ECharts.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.featureSet}>
+          <div className={styles.card}>
+            <img src={require('../img/piechart.png')} alt='piechart' className={styles.smallImg}></img>
+            <div className={styles.smallIntro}>
+              <h1 className={styles.featureDescription}>Piechart</h1>
+              <p className={styles.featureExplanation}>View composition of portfolio.</p>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <img src={require('../img/news.png')} alt='news' className={styles.smallImg}></img>
+            <div className={styles.smallIntro}>
+              <h1 className={styles.featureDescription}>Latest News</h1>
+              <p className={styles.featureExplanation}>A list of links to relevant news articles. News articles are scraped from Google News using Playwright.</p>
+            </div>
+          </div>
+        </div>
+
+      </section>
+    </div>
   )
 }
 
